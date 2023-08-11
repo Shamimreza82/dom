6c259c593fe5fd6194d008d6514e8708355b41b4
Shamimreza82 <shamimrezaone@gmail.com>
2023-08-12 00:14:31 +0600

    class practies

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,18 @@
-console.log(document);
+// const para = document.getElementById('para'); 
+// para.style.color = "red"; 
+// para.style.fontSize = "20px"
+// // console.log(para)
+
+
+// const  headers = document.getElementsByClassName("header"); 
+
+// const name = document.getElementsByTagName("h1");  
+// console.log (document)
+// ; 
+
+
+const section = document.querySelectorAll ('.section li'); 
+// console.log(section)
+for (const li of section){
+    console.log(li. innerText)
+}
