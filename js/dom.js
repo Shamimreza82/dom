003ff5800d5b0038633a3348b233956d1a07778e
Shamimreza82 <shamimrezaone@gmail.com>
2023-08-12 16:08:42 +0600

    update code

--- js/dom.js
+++ js/dom.js
@@ -1,18 +1,85 @@
-// const para = document.getElementById('para'); 
-// para.style.color = "red"; 
-// para.style.fontSize = "20px"
-// // console.log(para)
+// // const para = document.getElementById('para'); 
+// // para.style.color = "red"; 
+// // para.style.fontSize = "20px"
+// // // console.log(para)
 
 
-// const  headers = document.getElementsByClassName("header"); 
+// // const  headers = document.getElementsByClassName("header"); 
 
-// const name = document.getElementsByTagName("h1");  
-// console.log (document)
-// ; 
+// // const name = document.getElementsByTagName("h1");  
+// // console.log (document)
+// // ; 
 
 
-const section = document.querySelectorAll ('.section li'); 
-// console.log(section)
-for (const li of section){
-    console.log(li. innerText)
-}
+// // const section = document.querySelectorAll ('.section li'); 
+// // // console.log(section)
+// // for (const li of section){
+// //     console.log(li. innerText) 
+// // } 
+
+
+// // const sections = document.querySelectorAll('section'); 
+// // for (let section of sections){
+// //     console.log(section)
+// //     section.style. backgroundColor = "red"
+// //     section.style.color = "white"
+// //     section. style. textAlign = "center"
+// //     section.style. paddingTop = "40px"
+// // }
+
+
+// const nameList = document.getElementById('section'); 
+
+// const li = document.createElement ('li')
+// li.innerText = 'bangladesh'
+
+// nameList.appendChild(li)
+
+
+
+
+// const boddConatainer = document.getElementById ('body-contain')
+
+// const section = document.createElement ('section'); 
+// const h1 = document.createElement ('h1'); 
+// h1.innerText = "shamim Reza"
+// section.appendChild (h1); 
+
+// const ul = document.createElement ('ul');
+// const li1 = document.createElement ('li'); 
+// li1.innerText = "bangladesh"
+// ul.appendChild (li1);
+
+// const li2 = document.createElement ('li'); 
+// li2.innerText = "india"
+// ul.appendChild (li2);
+
+// const li3 = document.createElement ('li'); 
+// li3.innerText = "napal"
+// ul.appendChild (li3);
+
+// section.appendChild (ul)
+// boddConatainer.appendChild(section);
+
+
+// // innner html 
+
+
+// const dressSection = document.createElement ('section'); 
+// dressSection.innerHTML =  `
+//     <h1> My dress Section </h1> 
+//     <ul> 
+//         <li> shirt </li> 
+//         <li> shirt </li> 
+//         <li> shirt </li>
+//     </ul> 
+// `
+
+// boddConatainer.appendChild (dressSection);
+
+
+const parent = document.getElementById("parent");
+let childNodes = parent.childNodes;
+console.log(childNodes.length); // let's assume "2"
+parent.appendChild(document.createElement("div"));
+console.log(childNodes.length); // outputs "3"
