5b643a3341171c6a7b9a6ac11086fc65d2eeb613
Shamimreza82 <shamimrezaone@gmail.com>
2023-08-16 02:47:18 +0600

    update code

---
+++ preactis Task/js/task.js
@@ -0,0 +1,34 @@
+
+// document.getElementById('btn-discount').addEventListener('click', function () {
+
+//     const discount = document.getElementById('price');
+//     const getInnerText = discount.innerText;
+//     discount.innerText = `Wow!! You Got 30% discount: 
+//                             (Pay: 700 Taka)`;
+
+//     const discountDisplay = document.getElementById('discount-display');
+//     const displeyHide = discountDisplay.innerText;
+//     discountDisplay.innerText = "";
+// })
+
+
+document.getElementById('coupon-btn').addEventListener('click', function () {
+
+    const couponInput = document.getElementById('coupon-input');
+    const couponInputValue = couponInput.value;
+
+    if (couponInputValue === "DISC30") {
+        const addDiscount = document.getElementById('add-discount');
+        const addDiscountValue = addDiscount.innerText;
+        addDiscount.innerText = `Wow!! You get 30% discount
+                                 (Paid: 700)`;
+        addDiscount.style.color = "green"
+    }
+    else {
+        const addDiscount = document.getElementById('add-discount');
+        const addDiscountValue = addDiscount.innerText;
+        addDiscount.innerText = "Please Provide Valid Coupon";
+        addDiscount.style.color = "red"
+    }
+
+})
